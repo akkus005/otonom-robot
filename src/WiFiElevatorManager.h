0bed8661e7fc62c2c3513044f5375142d2578656
akkus005 <m.mert.akkus05@gmail.com>
2026-09-23 20:15:16 +0200

    Update WiFiElevatorManager.h

--- src/WiFiElevatorManager.h
+++ src/WiFiElevatorManager.h
@@ -1,37 +1,30 @@
 /*
- * Proje: Okul Robotu - Wi-Fi & Fingerbot Asansör Yöneticisi
+ * Proje: Okul Robotu - Wi-Fi AP ve Web Server Yöneticisi (PRO SÜRÜM)
  * Dosya: src/WiFiElevatorManager.h
  */
 
 #ifndef WIFI_ELEVATOR_MANAGER_H
 #define WIFI_ELEVATOR_MANAGER_H
 
-#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
-#include <HTTPClient.h>
 #include "ESP32_Config.h"
 
 class WiFiElevatorManager {
 private:
     WebServer server;
-    uint8_t currentTargetFloor;
-    bool elevatorTriggerPending;
+    int targetFloor;
 
+    void setupRoutes();
     void handleRoot();
+    void handleMove();
     void handleSetTarget();
 
 public:
     WiFiElevatorManager();
-    
     void begin();
     void handleClient();
-    
-    // Fingerbot/SwitchBot Cihazına Tuşa Basma İsteği Gönderir
-    bool triggerFingerbot(const char* fingerbotIP);
-    
-    uint8_t getTargetFloor();
-    void resetElevatorTrigger();
+    int getTargetFloor();
 };
 
 #endif // WIFI_ELEVATOR_MANAGER_H
